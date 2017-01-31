0f1177cfceb30982f63fe3125c4a97c381619ab6
Brian Washington <bwash34@gmail.com>
2017-01-30 23:37:29 -0600

    correct error text

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,11 +1,11 @@
-$('form#addReview').submit(function(e) {
+$('#addReview').submit(function(e) {
   $('.alert.alert-danger').hide();
 
   if(!$('input#name').val() || !$('select#rating').val() || !$('textarea#review').val()) {
     if($('.alert.alert-danger').length) {
       $('.alert.alert-danger').show();
     } else {
-      $(this).prepend('<div role="alert" class="alert alert-danger">Correct error</div>');
+      $(this).prepend('<div role="alert" class="alert alert-danger">All fields required, please try again</div>');
     }
     return false;
   }
